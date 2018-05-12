10b1b306d7c23b673ecb11e4a77ca9e8119697aa
Janusz Szubert <theinvalidnonce@gmail.com>
2018-05-12 18:38:15 -0400

    Connect Apollo client in App.jq

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import './App.css';
 
+//components
+import BookList from './components/BookList'
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloClient client={client}>
+        <div className='App'>
+          <h1>Reading List App<h1>
+          <BookList />
+        </div>
+      </ApolloClient>
     );
   }
 }
